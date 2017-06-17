229e59f72b2e9737ef58c68b8c57ca4c8f071523
FelixWinterstein <f.winterstein12@imperial.ac.uk>
2017-06-17 20:02:20 +0100

    new kernel file name

--- CrowdSimC5/generate_hardware.sh
+++ CrowdSimC5/generate_hardware.sh
@@ -1,1 +1,1 @@
-aoc -v -g --board c5soc --report -l ../common/rtl_src/custom_library.aoclib CrowdSim.aoco -o CrowdSim_release.aocx --fp-relaxed
+aoc -v -g --board c5soc --report -l ../common/rtl_src/custom_library.aoclib CrowdSim.aoco -o CrowdSim.aocx --fp-relaxed
